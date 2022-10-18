49eb90208a501b5e4cfeab7da570b3185fab560b
cy <cy@yunnk.com>
2022-10-18 09:18:58 +0800

    feat: nacos消息驱动 & sentinel限流、降级

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/example/controller/FlowLimitController.java
@@ -0,0 +1,32 @@
+package com.example.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class FlowLimitController {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @GetMapping("/testA")
+    public String testA() {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        return "------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+        //暂停几秒钟线程
+        int a = 10 / 0;
+        log.info("testD 测试RT");
+        return "------testD";
+    }
+}
